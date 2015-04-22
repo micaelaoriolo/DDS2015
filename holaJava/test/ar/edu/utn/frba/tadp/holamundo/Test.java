743e5226b41e229036d1d0ce895c776af32bdff7
Micaela Oriolo <micaelaoriolo@gmail.com>
2015-04-22 20:18:03 -0300

    Empiezo con colecciones.

--- holaJava/test/ar/edu/utn/frba/tadp/holamundo/Test.java
+++ holaJava/test/ar/edu/utn/frba/tadp/holamundo/Test.java
@@ -1,17 +1,30 @@
 package ar.edu.utn.frba.tadp.holamundo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import ar.edu.utn.frba.tadp.entes.Nombrable;
 import ar.edu.utn.frba.tadp.entes.Persona;
+
 
 public class Test {
 	
 	public static void main(String[] args) {
 		
+		
+		Collection<Nombrable> nombrables = getNombrables();
 		Recepcionista recepcionista = makeRecepcionista();
+		for(Nombrable nombrable : nombrables)
+		{
+			recepcionista.saludar(nombrable);
+		}
+		
+		
+		/*Recepcionista recepcionista = makeRecepcionista();
 		Nombrable nombrable = new Mundo();
 		recepcionista.saludar(nombrable);
 		nombrable = new Persona ("José");
-		recepcionista.saludar(nombrable);
+		recepcionista.saludar(nombrable);*/
 		
 		
 		
@@ -23,6 +36,12 @@ public class Test {
 		rF.saludar(mundo);*/
 	}
 	
+	private static Collection<Nombrable> getNombrables() {
+		Collection<Nombrable> nombrables = new ArrayList<Nombrable>();
+		
+		return null;
+	}
+
 	private static Recepcionista makeRecepcionista() {
 		return new RecepcionistaClasico();
 	}
